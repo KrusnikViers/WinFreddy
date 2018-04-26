85c8cf053b00f853c597b6ff6f61b9721837d3e3
Ivan Chernousov <civ-viers@yandex-team.ru>
2018-04-26 20:11:41 +0300

    Remove unnecessary packing CMake code

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -16,31 +16,3 @@ macro (set_target_strict_compilation_params TARGET_NAME)
             "Unknown compiler! Strict compilation not set for ${TARGET_NAME}.")
   endif()
 endmacro()
-
-# Creates C resources file from files in given directory
-macro (make_embedded_resources resources_directory output_filename)
-  file(WRITE ${output_filename}.cpp
-       "#include \"${output_filename}.h\"\n"
-       "// This file is generated automatically. Do not edit!\n"
-       "// Content is taken from ${resources_directory}\n\n")
-  file(WRITE ${output_filename}.h
-       "#pragma once\n"
-       "// This file is generated automatically. Do not edit!\n"
-       "// Content is taken from ${resources_directory}\n\n")
-  file(GLOB resource_files ${resources_directory}/*)
-  foreach(resource_file ${resource_files})
-    # Read and convert filename to C identifier.
-    string(REGEX MATCH "([^/]+)$" filename ${resource_file})
-    string(REGEX REPLACE "\\.| |-" "_" filename ${filename})
-    # Read and convert file data to C compatible format.
-    file(READ ${resource_file} hex_data HEX)
-    string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," hex_data ${hex_data})
-    # Append data to output file.
-    file(APPEND ${output_filename}.cpp
-         "unsigned char ${filename}[] = {${hex_data}0x00};\n"
-         "unsigned ${filename}_size = sizeof(${filename});\n")
-    file(APPEND ${output_filename}.h
-         "extern unsigned char ${filename}[];\n"
-         "extern unsigned ${filename}_size;\n\n")
-  endforeach()
-endmacro()
